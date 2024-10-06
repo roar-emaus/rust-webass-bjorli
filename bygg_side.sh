8197ebc416f3c31f38fcb8ba3cb1a44ac9b20dfe
Roar Emaus <roar@emaus.no>
2024-10-06 13:24:31 +0200

    endre byggeskript til å inkludere versjon

--- bygg_side.sh
+++ bygg_side.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/bash
 
+
+# Build with wasm-pack
 pushd bjorli
 wasm-pack build --target web
 popd
 
+# Copy files to the 'side' directory
 rm -r side
 mkdir side
-
-cp -r bjorli/index.html bjorli/pkg bjorli/style.css side/
+cp -r bjorli/index.html bjorli/pkg bjorli/style.css bjorli/bever.css side/
 rm side/pkg/.gitignore
 
+# File containing the version number
+HTML_FILE="side/index.html"
 
+# Extract the current version number from the HTML file
+VERSION=$(grep -o 'version=[0-9]*' $HTML_FILE | grep -o '[0-9]*')
+
+# Check if version exists, if not, set to 0
+if [ -z "$VERSION" ]; then
+    VERSION=0
+fi
+
+# Increment the version number
+NEW_VERSION=$((VERSION + 1))
+
+# Update the version number in the HTML file
+sed -i "s/version=$VERSION/version=$NEW_VERSION/" $HTML_FILE
+
+echo "Build version: $NEW_VERSION"
